27874d8b1b65495b4b080b5357ce47dbdbe7bfed
farzansaleem10 <farzansaleem103@gmail.com>
2024-11-29 12:03:32 +0530

    29/11/24

--- cll.c
+++ cll.c
@@ -42,7 +42,7 @@ int main()
 }
 void insert()
 {
-    int n, x, s, count = 1;
+    int n, x, s;
     printf("where do you want to insert\n1.beginning\n2.end\n3.anywhere\n");
     scanf("%d", &x);
     switch (x)
@@ -64,7 +64,7 @@ void insert()
         else
         {
             head = new;
-            new->next = head;
+            new->next = new;
         }
         break;
     case 2:
@@ -74,17 +74,17 @@ void insert()
         new->data = n;
         if (head != NULL)
         {
-            new->next = head;
+
             for (temp = head; temp->next != head; temp = temp->next)
             {
             }
             temp->next = new;
-            new->next=head;
+            new->next = head;
         }
         else
         {
             head = new;
-            new->next = head;
+            new->next = new;
         }
         break;
     case 3:
@@ -96,17 +96,6 @@ void insert()
         scanf("%d", &s);
         new->data = n;
         temp = head;
-        while (temp->next != head)
-        {
-            count++;
-            temp = temp->next;
-        }
-        if (s > count)
-        {
-            printf("Out of range!!");
-            return;
-        }
-
         if (s == 1)
         {
             if (head == NULL)
@@ -128,14 +117,14 @@ void insert()
         {
             temp = head;
 
-            for (int i = 1; i < s-1 ; i++)
+            for (int i = 1; i < s - 1; i++)
             {
                 temp = temp->next;
             }
             new->next = temp->next;
             temp->next = new;
-
-        }break;
+        }
+        break;
     }
 }
 
@@ -145,16 +134,121 @@ void display()
     {
         printf("data is empty");
     }
-    temp = head;
-    do
+    else{
+    for (temp = head; temp->next != head; temp = temp->next)
     {
         printf("%d\t", temp->data);
-        temp = temp->next;
-    } while (temp != head);
+    }
+    printf("%d", temp->data);
+    }
 }
 void search()
 {
+    int n, flag = 0;
+    printf("Enter the element to search: ");
+    scanf("%d", &n);
+    if (head == NULL)
+    {
+        printf("List is empty");
+    }
+    else
+    {
+        for (temp = head; temp->next != head; temp = temp->next)
+        {
+            if (temp->data == n)
+            {
+                flag = 1;
+                break;
+            }
+        }
+        if (temp->data == n)
+        {
+            flag = 1;
+        }
+        if (flag == 0)
+        {
+            printf("Element not found");
+        }
+        else
+        {
+            printf("Element found");
+        }
+    }
 }
 void delete()
 {
+    int x, s;
+    printf("where do you want to delete\n1.beginning\n2.end\n3.anywhere\n");
+    scanf("%d", &x);
+    switch (x)
+    {
+    case 1:
+        if (head == NULL)
+        {
+            printf("List is empty");
+        }
+        else if (head->next == head)
+        {
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            temp = head;
+            while (temp->next != head)
+            {
+                temp = temp->next;
+            }
+            temp->next = head->next;
+            head = head->next;
+        }
+        break;
+    case 2:
+        if (head == NULL)
+        {
+            printf("List is empty");
+        }
+        else if (head->next == head)
+        {
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            struct node *prev = NULL;
+            for (temp = head; temp->next != head; temp = temp->next)
+            {
+                prev = temp;
+            }
+            prev->next = temp->next;
+            free(temp);
+        }
+        break;
+    case 3:
+        printf("enter the position to delete: ");
+        scanf("%d", &s);
+        if (head == NULL)
+        {
+            printf("List is empty");
+        }
+        else if (head->next == head)
+        {
+            free(head);
+            head = NULL;
+        }
+        else
+        {
+            temp = head;
+            struct node *prev = NULL;
+
+            for (int i = 1; i < s; i++)
+            {
+                prev = temp;
+                temp = temp->next;
+            }
+            prev->next = temp->next;
+            free(temp);
+        }
+        break;
+    }
 }
