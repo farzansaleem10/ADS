21a933ef06363bd2dd7c058fa64cf12d70902ac0
bhavanatheruvath <99276236+bhavanatheruvath@users.noreply.github.com>
2025-01-05 21:09:02 +0530

    Update kruskal.c

--- kruskal.c
+++ kruskal.c
@@ -1,87 +1,76 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <limits.h>
 
-#define MAX_NODES 100
+#define size 50
+int i, j, A[size][size], parent[size], u, v, n;
 
-int DFS(int node, int n, int graph[][MAX_NODES], int visited[], int recStack[], int parent);
-
-
-void main() 
+int find(int vertex)
 {
-    int n;
-    printf("Enter the number of nodes: ");
-    scanf("%d", &n);
-
-    // Read the Adjacency matrix
-    int A[MAX_NODES][MAX_NODES];
-    printf("Enter the adjacency matrix: \n");
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            scanf("%d", &A[i][j]);
-            // If there is no edge between two nodes
-            if (!A[i][j])
-                A[i][j] = INT_MAX;
-        }
+    if (parent[vertex] != vertex)
+    {
+        parent[vertex] = find(parent[vertex]);
     }
+    return parent[vertex];
+}
 
-    int mst[MAX_NODES][MAX_NODES] = {0};
-    int edge_count = 0, cost = 0;
-    printf("The edges in the Minimum Spanning Tree are: \n");
-    while (edge_count < n - 1) {
-        // Find the smallest edge
-        int min = INT_MAX, u, v;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (A[i][j] < min) {
+void Union(int u, int v)
+{
+    int rootu = find(u);
+    int rootv = find(v);
+    parent[rootv] = rootu;
+}
+
+void kruskal()
+{
+    for (i = 0; i < n; i++)
+    {
+        parent[i] = i;
+    }
+    int e = 1, cost = 0;
+    while (e < n)
+    {
+        int min = INT_MAX;
+        for (i = 0; i < n; i++)
+        {
+            for (j = 0; j < n; j++)
+            {
+                if (A[i][j] < min)
+                {
                     min = A[i][j];
-                    u = i, v = j;
+                    u = i;
+                    v = j;
                 }
             }
         }
-
-        // Add the smallest edge to the MST and remove it from the graph
-        mst[u][v] = min;
-        A[u][v] = INT_MAX;
-        
-        // If the added edge forms a cycle in the MST, remove it 
-        for (int i = 0; i < n; i++) {
-            int visited[MAX_NODES] = {0}, recStack[MAX_NODES] = {0};
-            if (DFS(i, n, mst, visited, recStack, -1)) {
-                mst[u][v] = 0;
-                break;
-            }
-        }
-
-        // Print each unique edge ({a, b} is unique if {b, a} is not already present)
-        if (!mst[v][u] && mst[u][v] != 0) {
+        if (find(u) != find(v))
+        {
+            printf("{%d,%d}=%d\n", u, v, min);
             cost += min;
-            printf("{%d, %d} = %d\n", u, v, mst[u][v]);
-            edge_count++;
+            Union(u, v);
+            e++;
         }
+        A[u][v] = A[v][u] = INT_MAX;
     }
-    printf("Minimum Cost: %d\n", cost);
+    printf("Total cost: %d", cost);
 }
 
-
-// DFS Function: To detect Cycle in a Graph
-int DFS(int node, int n, int graph[][MAX_NODES], int visited[], int recStack[], int parent) {
-    if (!visited[node]) {
-        // recStack is used to keep track of nodes in the current path of traversal
-        visited[node] = recStack[node] = 1;
-
-        for (int i = 0; i < n; i++) {
-            if (graph[node][i] != 0) {
-                // Recursively calling the function on each unvisited node
-                if (!visited[i])
-                     DFS(i, n, graph, visited, recStack, node);
-                // If the node is already visited, and the node is not the parent of the current node (2)
-                else if (recStack[i] && i != parent)
-                    return 1;
+int main()
+{
+    printf("Enter the number of nodes: ");
+    scanf("%d", &n);
+    printf("Insert adjacency matrix:\nNote:insert weight if the nodes have edge bw them else insert 0\n");
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < n; j++)
+        {
+            printf("A[%d][%d] : ", i, j);
+            scanf("%d", &A[i][j]);
+            if (A[i][j] == 0)
+            {
+                A[i][j] = INT_MAX;
             }
         }
     }
-    // Remove the node from the recursion stack
-    recStack[node] = 0;
+    kruskal();
     return 0;
 }
