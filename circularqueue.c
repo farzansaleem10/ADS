be2df4e105bad1e5e70d7790230ba90ff85528e8
bhavanatheruvath <99276236+bhavanatheruvath@users.noreply.github.com>
2025-01-12 11:39:58 +0530

    Update circularqueue.c

--- circularqueue.c
+++ circularqueue.c
@@ -1,105 +1,77 @@
-#include <stdio.h>
-
-#define size 5
-
-
+#include<stdio.h>
+#include<stdlib.h>
+#define size 6
 void display();
-int enqueue();
-int dequeue();
-int IsEmpty();
-int IsFull();
 
+int queue[size],front=-1,rear=-1;
 
-int queue[size];
-int front = -1;
-int rear = -1;
-
-
-void main() {
-	while (1) {
-		int choice;
-		printf("Enter 0 to display, 1 to enqueue, 2 to dequeue, 5 to exit: ");
-		scanf("%d", &choice);
-		switch(choice) {
-			case 0:
-				display();
-				break;
-			case 1:
-				enqueue();
-				break;
-			case 2:
-				dequeue();
-				break;
-			case 5:
-				return;
-			default:
-				break;
-		}
-	}
+int isempty(){
+    if(front==-1)
+        return 1;
+    return 0;    
 }
 
+int isfull(){
+    if((rear+1)%size==front)
+        return 1;
+    return 0;    
+}
 
-void display() {
-    if (IsEmpty()) {
-        printf("Queue is empty!\n");
+void enque(){
+    int value;
+    if(isfull()){
+        printf("overflow!\n");
         return;
     }
-
-	for (int i = front; i != rear; i = (i + 1) % size)
-		printf("%d ", queue[i]);
-	printf("%d\n", queue[rear]);
-}
-
-
-int enqueue() {
-	if (IsFull()) {
-		printf("Overflow!\n");
-		return 1;
-	}
-
-	int value;
-	printf("Element: ");
-	scanf("%d", &value);
-	
-	if (IsEmpty())
-		front =  0;
-	rear = (rear + 1) % size;
-
-	queue[rear] = value;
+    printf("element: ");
+    scanf("%d",&value);
+    if(isempty()){
+        front=0;
+    }
+    rear=(rear+1)%size;
+    queue[rear]=value;
     display();
-	return value;
 }
 
-
-int dequeue() {
-	if (IsEmpty()) {
-		printf("Underflow!\n");
-		return 1;
-	}
-	int value = queue[front];
-	
-	// If only one element remains
-	if (front == rear) {
-		front = rear = -1;
-	}
-	// Otherwise, increment front
-	else {
-		front = (front + 1) % size;
-	}
-
-    display();
-	return value;
+void deque(){
+    if(isempty()){
+        printf("underflow!\n");
+        return;
+    }
+    if(front==rear){
+        front=rear=-1;
+    }
+    else{
+        front=(front+1)%size;
+    }
 }
 
-
-int IsFull() {
-	if ((rear + 1) % size == front)
-		return 1;
-	return 0;
+void display(){
+    if(isempty()){
+        printf("empty!!\n");
+        return;
+    }
+    for(int i=front;i!=rear;i=(i+1)%size){
+        printf("%d ",queue[i]);
+    }
+    printf("%d",queue[rear]);
 }
 
-int IsEmpty() {
-	if (front == -1)
-		return 1;
-	return 0;
+void main(){
+    int ch;
+    while(1){
+        printf("\n1.enque\t2.deque\t3.display\t4.exit\nchoice: ");
+        scanf("%d",&ch);
+        switch(ch){
+            case 1:enque();
+            break;
+            case 2:deque();
+            break;
+            case 3:display();
+            break;
+            case 4:exit(0);
+            break;
+            default:printf("invalid choice!\n");
+        }
+    }
 }
